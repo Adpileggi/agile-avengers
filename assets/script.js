705d4bd14b1e826bb126ec7ce514841de3252ef6
Anthony Pileggi <adpileggi@gmail.com>
2024-01-29 19:43:30 -0500

    added files and basic wireframe

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+// connect map api
+// display map in the map-el div
+// have pins in the map which when cliked will populate recipes
+
+
+// connect recipe api
+// have the recipe api search for state recipe when click event happens on the map?
+
+// populate search results to the recipe div
+
+// https://maps.googleapis.com/maps/api/staticmap?center=USA,markers=
